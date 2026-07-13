1d3e8a3b6cc32f4eddc6f7c753ea844d180b78d8
Henry Blanchette <henry@rybl.net>
2026-07-13 17:54:42 -0400

    setup rtn parser

--- src/Language/Rtn.hs
+++ src/Language/Rtn.hs
@@ -1,30 +1,67 @@
 module Language.Rtn where
 
+import Control.Monad (void)
 import Data.Text (Text)
-import Data.Tree (Tree (Node))
-import Data.Tree qualified as Tree
+import Data.Text.Lazy (LazyText)
 import Prettyprinter (Pretty (pretty))
 import Prettyprinter qualified as Pp
 import Prettyprinter.Util qualified as Pp
+import Text.Parsec (ParseError, SourceName, SourcePos, char, choice, getPosition, many, runParser)
+import Text.Parsec.Language
+import Text.Parsec.Text.Lazy (Parser)
+import Text.Parsec.Token qualified as Token
+import Utilities.Unsafe
 
 --------------------------------
 -- Syntax
 --------------------------------
 
-type Term ann = Tree.Tree (Label ann)
-
-pattern Leaf :: Label ann -> Term ann
-pattern Leaf l = Tree.Node l []
-
-instance {-# OVERLAPPING #-} Pretty (Term ann) where
-  pretty (Leaf l) = pretty l
-  pretty (Node l ts) = "(" <> pretty l <> (Pp.vsep . fmap pretty) ts <> ")"
-
-data Label ann = Label
-  { labelAnn :: ann,
-    labelValue :: Text
-  }
+data Term ann = Node ann Text [Term ann]
   deriving (Show, Eq, Ord, Functor, Foldable, Traversable)
 
-instance Pretty (Label ann) where
-  pretty l = Pp.reflow l.labelValue
+pattern Leaf :: ann -> Text -> Term ann
+pattern Leaf ann l = Node ann l []
+
+instance Pretty (Term ann) where
+  pretty (Leaf _ l) = pretty l
+  pretty (Node _ l ts) = "(" <> pretty l <> (Pp.vsep . fmap pretty) ts <> ")"
+
+--------------------------------
+-- Parsing
+--------------------------------
+
+type Ann = (SourcePos, SourcePos)
+
+-- | A label can contain any characters except whitespace and parentheses.
+parseLabel :: Parser Text
+parseLabel = todo "parse a label"
+
+parseWhiteSpaces :: Parser ()
+parseWhiteSpaces = todo "parse any amount of white-space"
+
+parseTerm :: Parser (Term Ann)
+parseTerm =
+  choice
+    [ do
+        p0 <- getPosition
+        (void . char) '('
+        l <- parseLabel
+        ts <- many parseTerm
+        (void . char) ')'
+        p1 <- getPosition
+        parseWhiteSpaces
+        pure $ Node (p0, p1) l ts,
+      do
+        p0 <- getPosition
+        l <- parseLabel
+        p1 <- getPosition
+        parseWhiteSpaces
+        pure $ Leaf (p0, p1) l
+    ]
+
+runParseTerm :: SourceName -> LazyText -> Either ParseError (Term Ann)
+runParseTerm = runParser parseTerm ()
+
+--------------------------------
+-- RtnParser
+--------------------------------
